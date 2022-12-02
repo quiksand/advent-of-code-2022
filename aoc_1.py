d288d964e58990ddccbfdbdf5b723092a1640c1c
Cody Schindler <cody.schindler@procore.com>
2022-12-02 15:51:21 -0800

    Solve second part of day 1

--- aoc_1.py
+++ aoc_1.py
@@ -4,6 +4,39 @@ import os
 import requests
 
 
+class TopNCalorieTracker:
+    def __init__(self, n):
+        self.n = n
+        self.tracker = [0] * n
+
+    def insert(self, calories):
+        tracker = []
+        idx = 0
+        while idx < n:
+            if calories > self.tracker[idx]:
+                tracker.append(calories)
+                tracker.extend(self.tracker[idx:-1])
+                break
+            else:
+                tracker.append(self.tracker[idx])
+            idx += 1
+        self.tracker = tracker
+
+    # def insert(self, calories):
+    #     def get_index():
+    #         for index, value in enumerate(self.tracker):
+    #             if calories >= value:
+    #                 continue
+    #             else:
+    #                 return index
+    #     index = get_index()
+    #     if index:
+    #         self.tracker = (self.tracker[:index] + [calories] + self.tracker[index:])[:n]
+
+    def total(self):
+        return sum(self.tracker)
+
+
 def get_input_data():
     dotenv.load_dotenv()
     session_token = os.getenv('SESSION_TOKEN')
@@ -25,7 +58,29 @@ def get_max_calories_carried_by_single_elf(food_list):
 
     return max_calories_carried_by_single_elf
 
+def get_calories_of_top_n_elves(n, food_list):
+    # Sure, we could just create a long list of sums, sort, and sum the top 3, but where's the fun in that?
+    calorie_tracker = TopNCalorieTracker(n)
+    total_calories_per_elf = 0
+    for food in food_list.splitlines():
+        if food == '':
+            calorie_tracker.insert(total_calories_per_elf)
+            total_calories_per_elf = 0
+        else:
+            total_calories_per_elf += int(food)
+    return calorie_tracker
+
+# Answer to part 1
+# if __name__ == '__main__':
+#     food_list = get_input_data()
+#     max_calories_carried_by_single_elf = get_max_calories_carried_by_single_elf(food_list)
+#     print(f'The top elf carries {max_calories_carried_by_single_elf} calories')
+
+# Answer to part 2
 if __name__ == '__main__':
+    n = 3
     food_list = get_input_data()
-    max_calories_carried_by_single_elf = get_max_calories_carried_by_single_elf(food_list)
-    print(max_calories_carried_by_single_elf)
+    calories_of_top_n_elves = get_calories_of_top_n_elves(n, food_list)
+    print(calories_of_top_n_elves.tracker)
+    print(f'The top elf carries {calories_of_top_n_elves.tracker[0]} calories')
+    print(f'The top {n} elves carry {calories_of_top_n_elves.total()} calories')
